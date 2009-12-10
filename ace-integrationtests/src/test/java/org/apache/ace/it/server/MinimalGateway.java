aea0d115f27461c7c783201db55087a2a6310905
Toni Menzel <tonit@apache.org>
2009-12-10 13:11:16 +0000

    ACE-68 Pax Exam Test Project which uses the ace-target-XX assemblies to configure itself. In the works (currently fails, but it is not in default reactor yet)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ace/trunk@889251 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ace-integrationtests/src/test/java/org/apache/ace/it/server/MinimalGateway.java
@@ -0,0 +1,76 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ace.it.server;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+
+import static junit.framework.Assert.*;
+import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.*;
+
+/**
+ * @author Toni Menzel
+ * @since Dec 10, 2009
+ */
+@RunWith( JUnit4TestRunner.class )
+public class MinimalGateway
+{
+
+    @Configuration
+    public Option[] config()
+    {
+        return options(
+            felix().version( "2.0.1" ),
+            // this is the launch configuration for our "target". Packed and referenced as a normal maven artifact.
+            scanPom( "jar:mvn:org.apache.ace/ace-target-devgateway/0.8.0-SNAPSHOT!/epom.xml" ),
+            autoWrap()
+        );
+    }
+
+    @Inject
+    BundleContext context;
+
+    @Test
+    public void resolve()
+    {
+        List<Bundle> notLoaded = new ArrayList<Bundle>();
+
+        // just check that all have been resolved.
+        for( Bundle bundle : context.getBundles() )
+        {
+            if( bundle.getState() != Bundle.ACTIVE )
+            {
+                System.err.println( "Bundle " + bundle.getLocation() + " has not been started." );
+                notLoaded.add( bundle );
+            }
+        }
+        if( notLoaded.size() > 0 )
+        {
+            fail( "Some bundles have not been started properly. See Log Messages for details." );
+        }
+
+    }
+}
