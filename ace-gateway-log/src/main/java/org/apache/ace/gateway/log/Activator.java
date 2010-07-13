7b935f0120a712eb5c642c01fb43f06e55c49cd2
Marcel Offermans <marrs@apache.org>
2010-07-13 19:46:51 +0000

    Fixes for the latest dependency manager snapshot.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ace/trunk@963834 13f79535-47bb-0310-9956-ffa450edef68

--- ace-gateway-log/src/main/java/org/apache/ace/gateway/log/Activator.java
+++ ace-gateway-log/src/main/java/org/apache/ace/gateway/log/Activator.java
@@ -31,7 +31,7 @@ import org.apache.ace.log.Log;
 import org.apache.ace.scheduler.constants.SchedulerConstants;
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
-import org.apache.felix.dm.service.Service;
+import org.apache.felix.dm.Service;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.service.cm.ConfigurationException;
