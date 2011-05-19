0b7ead347cbf2d7ca4bcc51b97993a9a97cc190c
Karl Pauls <pauls@apache.org>
2011-05-19 13:07:44 +0000

    delete tag
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ace/releases@1124731 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.ace.util-0.8.0-incubator/src/main/resources/run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call java -jar pax-runner.jar --ups --workingDirectory=. scan-dir:required-bundles scan-dir:ace-bundles scan-file:file:platform.properties %1 %2 %3
--- org.apache.ace.util-0.8.0-incubator/src/main/resources/run.sh
+++
@@ -1,1 +0,0 @@
-java -jar pax-runner.jar --ups --workingDirectory=. scan-dir:required-bundles scan-dir:ace-bundles scan-file:file:platform.properties
