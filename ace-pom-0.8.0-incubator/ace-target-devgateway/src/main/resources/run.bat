230619db70251a03ca61ee4453b3d9d7b42b930c
Karl Pauls <pauls@apache.org>
2011-05-19 09:41:53 +0000

    Delete tag
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ace/releases@1124643 13f79535-47bb-0310-9956-ffa450edef68

--- ace-pom-0.8.0-incubator/ace-target-devgateway/src/main/resources/run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call java -jar pax-runner.jar --autoWrap --workingDirectory=. scan-dir:ace-bundles scan-dir:required-bundles scan-file:file:conf/platform.properties %1 %2 %3
--- ace-pom-0.8.0-incubator/ace-target-devgateway/src/main/resources/run.sh
+++
@@ -1,1 +0,0 @@
-java -jar pax-runner.jar --autoWrap --workingDirectory=. scan-dir:ace-bundles scan-dir:required-bundles scan-file:file:conf/platform.properties $*
