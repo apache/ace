3e4164e6c50dceb2684e72cf82c653c044e6d2d1
Karl Pauls <pauls@apache.org>
2011-05-19 14:24:03 +0000

    [maven-release-plugin]  copy for tag org.apache.ace.client.repository.helper.configuration-0.8.0-incubator
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ace/releases@1124828 13f79535-47bb-0310-9956-ffa450edef68

---
+++ org.apache.ace.client.repository.helper.configuration-0.8.0-incubator/src/main/java/org/apache/ace/client/repository/helper/configuration/impl/Activator.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.ace.client.repository.helper.configuration.impl;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.apache.ace.client.repository.helper.ArtifactHelper;
+import org.apache.ace.client.repository.helper.ArtifactRecognizer;
+import org.apache.ace.client.repository.helper.configuration.ConfigurationHelper;
+import org.apache.ace.client.repository.object.ArtifactObject;
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Activator class for the Configuration ArtifactHelper.
+ */
+public class Activator extends DependencyActivatorBase {
+
+    @Override
+    public synchronized void init(BundleContext context, DependencyManager manager) throws Exception {
+        Dictionary<String, String> props = new Hashtable<String, String>();
+        props.put(ArtifactObject.KEY_MIMETYPE, ConfigurationHelper.MIMETYPE);
+        ConfigurationHelperImpl helperImpl = new ConfigurationHelperImpl();
+        manager.add(createComponent()
+            .setInterface(ArtifactHelper.class.getName(), props)
+            .setImplementation(helperImpl));
+        manager.add(createComponent()
+            .setInterface(ArtifactRecognizer.class.getName(), null)
+            .setImplementation(helperImpl));
+    }
+
+    @Override
+    public synchronized void destroy(BundleContext context, DependencyManager manager) throws Exception {
+        // Nothing to do
+    }
+}
