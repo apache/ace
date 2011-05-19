d0eba38638983cfa9e4036e62ea740c6657a71d1
Karl Pauls <pauls@apache.org>
2011-05-19 15:00:56 +0000

    [maven-release-plugin]  copy for tag org.apache.ace.obr.metadata-0.8.0-incubator
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ace/releases@1124885 13f79535-47bb-0310-9956-ffa450edef68

---
+++ org.apache.ace.obr.metadata-0.8.0-incubator/src/main/java/org/apache/ace/obr/metadata/MetadataGenerator.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.ace.obr.metadata;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface MetadataGenerator
+{
+
+    /**
+     * Generates the repository.xml based upon the new set of Bundles in the given directory. The xml is created
+     * as result of this method in the given directory in a file called repository.xml.
+     * This methods creates the file in an atomic fashion (this includes retrying to overwrite an existing file until success).
+     *
+     * @param directory the location where to store the newly created repository.xml
+     *
+     * @throws java.io.IOException If I/O problems occur when generating the new meta data index file.
+     */
+    public void generateMetadata(File directory) throws IOException;
+}
