b254bac63e6d5a9d05b1da886a7673a943f4a72f
Karl Pauls <pauls@apache.org>
2011-05-19 15:36:00 +0000

    [maven-release-plugin]  copy for tag org.apache.ace.nodelauncher.amazon-0.8.0-incubator
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ace/releases@1124948 13f79535-47bb-0310-9956-ffa450edef68

---
+++ org.apache.ace.nodelauncher.amazon-0.8.0-incubator/src/test/java/org/apache/ace/nodelauncher/amazon/PortParseTest.java
@@ -0,0 +1,43 @@
+package org.apache.ace.nodelauncher.amazon;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static org.apache.ace.test.utils.TestUtils.UNIT;
+
+public class PortParseTest {
+    @Test(groups = { UNIT })
+    public void testParsePortsFromString() throws Exception {
+        AmazonNodeLauncher instance = new AmazonNodeLauncher();
+        Assert.assertTrue(assertEquals(instance.parseExtraPorts("1,2,3"), new int[] {1, 2, 3}));
+        Assert.assertTrue(assertEquals(instance.parseExtraPorts(""), new int[] {}));
+        Assert.assertTrue(assertEquals(instance.parseExtraPorts("1 ,2 , 3 "), new int[] {1, 2, 3}));
+        Assert.assertTrue(assertEquals(instance.parseExtraPorts("800,900"), new int[] {800, 900}));
+    }
+    
+    @Test(groups = { UNIT })
+    public void testMergePorts() throws Exception {
+        AmazonNodeLauncher instance = new AmazonNodeLauncher();
+        Assert.assertTrue(assertEquals(instance.mergePorts(new int[] {1, 2}, new int[] {3, 4, 5}), new int[] {1, 2, 3, 4, 5}));
+        Assert.assertTrue(assertEquals(instance.mergePorts(new int[] {1}, new int[] {}), new int[] {1}));
+        Assert.assertTrue(assertEquals(instance.mergePorts(new int[] {}, new int[] {}), new int[] {}));
+        Assert.assertTrue(assertEquals(instance.mergePorts(new int[] {}, new int[] {1, 2, 3}), new int[] {1, 2, 3}));
+    }
+    
+    private boolean assertEquals(int[] a, int[] b) {
+        if (a == null && b == null) {
+            return true;
+        }
+        if (a == null) {
+            return false;
+        }
+        if (a.length != b.length) {
+            return false;
+        }
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
