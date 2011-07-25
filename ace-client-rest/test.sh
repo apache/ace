2ade374376413203f22073dbe4e8a59b96a94441
Marcel Offermans <marrs@apache.org>
2011-07-25 20:19:35 +0000

    ACE-151 updated the test script
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ace/trunk@1150882 13f79535-47bb-0310-9956-ffa450edef68

--- ace-client-rest/test.sh
+++ ace-client-rest/test.sh
@@ -12,16 +12,36 @@ echo "Workspace is ${WORK}"
 #curl -v -d "{ attributes: { name: 'feature-${RANDOM}', description: 'a random feature' }, tags: { generated: 'true'}}" ${WORK}/feature
 #curl -v -d "{ attributes: { name: 'feature-${RANDOM}', description: 'another random feature' }, tags: { generated: 'true'}}" ${WORK}/feature
 
-# Add a feature, a distribution and an association between the two
-echo "*** Adding feature, distribution and association..."
+echo "*** Adding artifact, feature, distribution, target and all associations..."
+
+RND=$RANDOM
+BSN=org.apache.bundle${RND}
+VERSION=1.0.0
+NAME=${BSN}-${VERSION}
+ART=`curl -v -d "{attributes: { artifactName: '${NAME}' , mimetype: 'application/vnd.osgi.bundle', Bundle-Name: '${BSN}', Bundle-SymbolicName: '${BSN}', Bundle-Version: '${VERSION}', url: 'http://localhost:8080/obr/${NAME}.jar', artifactDescription: 'coolio', processorPid: '' }, tags: { generated: 'true' }}" -w %{redirect_url} ${WORK}/artifact`
+ARTID=`echo ${ART##*/} | perl -MURI::Escape -lne 'print uri_unescape($_)'`
+echo "Artifact is ${ART} => ${ARTID}"
+
 FEAT=`curl -v -d "{ attributes: { name: 'feature-${RANDOM}', description: 'a feature' }, tags: {}}" -w %{redirect_url} ${WORK}/feature`
 FEATID=`echo ${FEAT##*/} | perl -MURI::Escape -lne 'print uri_unescape($_)'`
 echo "Feature is ${FEAT} => ${FEATID}"
+
 DIST=`curl -v -d "{ attributes: { name: 'distribution-${RANDOM}', description: 'a distribution' }, tags: {}}" -w %{redirect_url} ${WORK}/distribution`
 DISTID=`echo ${DIST##*/} | perl -MURI::Escape -lne 'print uri_unescape($_)'`
 echo "Distribution is ${DIST} => ${DISTID}"
-ASSOC=`curl -v -d "{ attributes: { leftEndpoint: '${FEATID}', leftCardinality: '1', rightEndpoint: '${DISTID}', rightCardinality: '1' }, tags: {}}" -w %{redirect_url} ${WORK}/feature2distribution`
-echo "Association is ${ASSOC}"
+
+TARGET=`curl -v -d "{ attributes: { id: 'target-${RANDOM}', autoapprove: 'true' }, tags: {}}" -w %{redirect_url} ${WORK}/target`
+TARGETID=`echo ${TARGET##*/} | perl -MURI::Escape -lne 'print uri_unescape($_)'`
+echo "Target is ${TARGET} => ${TARGETID}"
+
+ASSOC1=`curl -v -d "{ attributes: { leftEndpoint: '${ARTID}', leftCardinality: '1', rightEndpoint: '${FEATID}', rightCardinality: '1' }, tags: {}}" -w %{redirect_url} ${WORK}/artifact2feature`
+echo "Association is ${ASSOC1}"
+
+ASSOC2=`curl -v -d "{ attributes: { leftEndpoint: '${FEATID}', leftCardinality: '1', rightEndpoint: '${DISTID}', rightCardinality: '1' }, tags: {}}" -w %{redirect_url} ${WORK}/feature2distribution`
+echo "Association is ${ASSOC2}"
+
+ASSOC3=`curl -v -d "{ attributes: { leftEndpoint: '${DISTID}', leftCardinality: '1', rightEndpoint: '${TARGETID}', rightCardinality: '1' }, tags: {}}" -w %{redirect_url} ${WORK}/distribution2target`
+echo "Association is ${ASSOC3}"
 
 # Get a list of artifacts
 #curl ${WORK}/artifact
@@ -34,10 +54,6 @@ echo "Association is ${ASSOC}"
 #VERSION=1.0.0
 #NAME=${BSN}-${VERSION}
 #curl -v -d "{attributes: { artifactName: '${NAME}' , mimetype: 'application/vnd.osgi.bundle', Bundle-Name: '${BSN}', Bundle-SymbolicName: '${BSN}', Bundle-Version: '${VERSION}', url: 'http://localhost:8080/obr/${NAME}.jar', artifactDescription: 'coolio', processorPid: '' }, tags: { generated: 'true' }}" ${WORK}/artifact
-
-# Create a target
-TARGET=`curl -v -d "{ attributes: { id: 'target-${RANDOM}', autoapprove: 'true' }, tags: {}}" -w %{redirect_url} ${WORK}/target`
-echo "Target is ${TARGET}"
 
 # Commit the workspace
 echo "*** Committing workspace..."
