435c62d9ec2fbc136e56960b05a5a209a59459e4
Jan Willem Janssen <jawi@apache.org>
2012-08-07 11:28:57 +0000

    Use own logger, instead of duplicating the code.
    git-svn-id: https://svn.apache.org/repos/asf/ace/sandbox/marrs@1370186 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.ace.managementagent/src/org/apache/ace/managementagent/Activator.java
+++ org.apache.ace.managementagent/src/org/apache/ace/managementagent/Activator.java
@@ -10,11 +10,8 @@ import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
-import org.osgi.framework.ServiceReference;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
-import org.osgi.service.log.LogService;
 
 public class Activator extends DependencyActivatorBase {
 
@@ -63,55 +60,9 @@ public class Activator extends DependencyActivatorBase {
     @Override
     public void init(BundleContext context, DependencyManager manager) throws Exception {
         if (m_logToConsole) {
-        	manager.add(createComponent()
-        		.setInterface(LogService.class.getName(), null)
-        		.setImplementation(new LogService() {
-        		    private String[] LEVEL = { "", "Error", "Warn ", "Info ", "Debug" };
-
-        		    public void log(int level, String message) {
-        		        log(null, level, message, null);
-        		    }
-
-        		    public void log(int level, String message, Throwable throwable) {
-        		        log(null, level, message, throwable);
-        		    }
-
-        		    public void log(ServiceReference reference, int level, String message) {
-        		        log(reference, level, message, null);
-        		    }
-
-        		    public void log(ServiceReference reference, int level, String message, Throwable throwable) {
-        		        String bundle = " [   ]";
-        		        String service = " ";
-        		        if (reference != null) {
-        		            bundle = "00" + reference.getBundle().getBundleId();
-        		            bundle = " [" + bundle.substring(bundle.length() - 3) + "]";
-        		            Object objectClass = reference.getProperty(Constants.OBJECTCLASS);
-        		            if (objectClass instanceof String[]) {
-        		                StringBuffer buffer = new StringBuffer();
-        		                String[] objClassArr = ((String[]) objectClass);
-        		                for (int i = 0; i < objClassArr.length; i++) {
-        		                    String svc = objClassArr[i];
-        		                    if (buffer.length() > 0) {
-        		                        buffer.append(';');
-        		                    }
-        		                    buffer.append(svc);
-        		                    service = buffer.toString() + ": ";
-        		                }
-        		            }
-        		            else {
-        		                service = objectClass.toString() + ": ";
-        		            }
-        		        }
-        		        System.out.println("[" + LEVEL[level] + "]" + bundle + service + message);
-        		        if (throwable != null) {
-        		            throwable.printStackTrace();
-        		        }
-        		    }
-				})
-        	);
+            new org.apache.ace.consolelogger.Activator().start(context);
         }
-        
+
         for (int i = 0; i < m_activators.length; i++) {
             BundleActivator a = m_activators[i];
             // start the bundle unless there is a system property with the same package name as
