apache/ace

commit 10d7140b4d76e2a925c907a2ab1862fe1a67d129

Improved logging and exception messages when storing an artifact in the OBR fails.
git-svn-id: https://svn.apache.org/repos/asf/ace/trunk@1498701 13f79535-47bb-0310-9956-ffa450edef68

org.apache.ace.obr/src/org/apache/ace/obr/storage/file/BundleFileStore.java
@@ -96,14 +96,15 @@ public class BundleFileStore implements BundleStore, ManagedService {
             metaData = ResourceMetaData.getArtifactMetaData(fileName);
         }
         if (metaData == null) {
-            throw new IOException("Not a valid bundle and no filename found");
+            throw new IOException("Not a valid bundle and no filename found (filename = " + fileName + ")");
         }
 
         File storeLocation = getResourceFile(metaData);
         if (storeLocation == null) {
-            throw new IOException("Failed to store resource");
+            throw new IOException("Failed to store resource (filename = " + fileName + ")");
         }
         if (storeLocation.exists()) {
+            m_log.log(LogService.LOG_ERROR, "Resource already existed in OBR (filename = " + fileName + ")");
             return null;
         }
 

commit 564e8800b53ccde046f7149446af870a03590e70

Fixed itest npe failures introduced in r1498701
git-svn-id: https://svn.apache.org/repos/asf/ace/trunk@1500605 13f79535-47bb-0310-9956-ffa450edef68

org.apache.ace.obr/src/org/apache/ace/obr/storage/file/BundleFileStore.java
@@ -89,6 +89,8 @@ public class BundleFileStore implements BundleStore, ManagedService {
 
     public String put(InputStream data, String fileName) throws IOException {
 
+        if(fileName == null)
+            fileName = "";
         File tempFile = downloadToTempFile(data);
 
         ResourceMetaData metaData = ResourceMetaData.getBundleMetaData(tempFile);
@@ -101,10 +103,10 @@ public class BundleFileStore implements BundleStore, ManagedService {
 
         File storeLocation = getResourceFile(metaData);
         if (storeLocation == null) {
-            throw new IOException("Failed to store resource (filename = " + fileName + ")");
+            throw new IOException("Failed to store resource (filename = " +  fileName  + ")");
         }
         if (storeLocation.exists()) {
-            m_log.log(LogService.LOG_ERROR, "Resource already existed in OBR (filename = " + fileName + ")");
+            m_log.log(LogService.LOG_ERROR, "Resource already existed in OBR (filename = " +  fileName  + ")");
             return null;
         }
 

org.apache.ace.obr/test/org/apache/ace/obr/storage/file/BundleFileStoreTest.java
@@ -39,6 +39,7 @@ import org.apache.ace.test.utils.FileUtils;
 import org.apache.ace.test.utils.TestUtils;
 import org.osgi.framework.Constants;
 import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.log.LogService;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -66,6 +67,9 @@ public class BundleFileStoreTest {
         Properties props = new Properties();
         props.put(OBRFileStoreConstants.FILE_LOCATION_KEY, m_directory.getAbsolutePath());
         m_bundleStore.updated(props);
+
+        // set a null object on for log
+        TestUtils.configureObject(m_bundleStore, LogService.class);
 
         // create a mock MetadataGenerator
         m_metadata = new MockMetadataGenerator();
@@ -258,7 +262,7 @@ public class BundleFileStoreTest {
         assert filePath2 == null;
     }
 
-    @Test(groups = { UNIT }, expectedExceptions = { IOException.class }, expectedExceptionsMessageRegExp = "Not a valid bundle and no filename found")
+    @Test(groups = { UNIT }, expectedExceptions = { IOException.class }, expectedExceptionsMessageRegExp = "Not a valid bundle and no filename found.*")
     public void putBundleFail() throws Exception {
         File bundle = createTmpResource(null, "1.0.0");
         String filePath = m_bundleStore.put(new FileInputStream(bundle), null);
@@ -294,13 +298,13 @@ public class BundleFileStoreTest {
         assert file.exists();
     }
 
-    @Test(groups = { UNIT }, expectedExceptions = { IOException.class }, expectedExceptionsMessageRegExp = "Not a valid bundle and no filename found")
+    @Test(groups = { UNIT }, expectedExceptions = { IOException.class }, expectedExceptionsMessageRegExp = "Not a valid bundle and no filename found.*")
     public void putArtifactFail1() throws Exception {
         File bundle = createTmpResource(null, null);
         m_bundleStore.put(new FileInputStream(bundle), null);
     }
 
-    @Test(groups = { UNIT }, expectedExceptions = { IOException.class }, expectedExceptionsMessageRegExp = "Not a valid bundle and no filename found")
+    @Test(groups = { UNIT }, expectedExceptions = { IOException.class }, expectedExceptionsMessageRegExp = "Not a valid bundle and no filename found.*")
     public void putArtifactFail2() throws Exception {
         File bundle = createTmpResource(null, null);
         m_bundleStore.put(new FileInputStream(bundle), "");