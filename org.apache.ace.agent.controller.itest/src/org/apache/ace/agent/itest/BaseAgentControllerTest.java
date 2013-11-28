d811ac72de6616d21215fba1ae488c9f687052de
Jan Willem Janssen <jawi@apache.org>
2013-11-28 11:01:13 +0000

    ACE-433 - moved custom controller itest to its own project:
    - this is in preparation for the new way custom controllers should be
      registered and used.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ace/trunk@1546342 13f79535-47bb-0310-9956-ffa450edef68

---
+++ org.apache.ace.agent.controller.itest/src/org/apache/ace/agent/itest/BaseAgentControllerTest.java
@@ -0,0 +1,161 @@
+package org.apache.ace.agent.itest;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.ace.agent.AgentConstants;
+import org.apache.ace.agent.ConfigurationHandler;
+import org.apache.ace.builder.DeploymentPackageBuilder;
+import org.apache.ace.it.IntegrationTestBase;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Version;
+
+import aQute.lib.osgi.Builder;
+import aQute.lib.osgi.Jar;
+
+public abstract class BaseAgentControllerTest extends IntegrationTestBase {
+
+    protected static class TestBundle {
+        private final File m_file;
+
+        public TestBundle(String name, Version version, String... headers) throws Exception {
+            m_file = createBundle(name, version, headers);
+        }
+
+        public File getFile() {
+            return m_file;
+        }
+    }
+
+    protected static class TestPackage {
+        private final String m_name;
+        private final Version m_version;
+        private final File m_file;
+
+        public TestPackage(String name, Version version, TestBundle... bundles) throws Exception {
+            m_name = name;
+            m_version = version;
+
+            File[] files = new File[bundles.length];
+            for (int i = 0; i < bundles.length; i++) {
+                files[i] = bundles[i].getFile();
+            }
+            m_file = createPackage(m_name, m_version, files);
+        }
+
+        public String getName() {
+            return m_name;
+        }
+
+        public File getFile() {
+            return m_file;
+        }
+
+        public Version getVersion() {
+            return m_version;
+        }
+    }
+
+    protected static File createBundle(String bsn, Version version, String... headers) throws Exception {
+        Builder b = new Builder();
+
+        try {
+            b.setProperty("Bundle-SymbolicName", bsn);
+            b.setProperty("Bundle-Version", version.toString());
+            for (int i = 0; i < headers.length; i += 2) {
+                b.setProperty(headers[i], headers[i + 1]);
+            }
+            b.setProperty("Include-Resource", "bnd.bnd"); // prevent empty jar bug
+
+            Jar jar = b.build();
+            jar.getManifest(); // Not sure whether this is needed...
+
+            File file = File.createTempFile("testbundle", ".jar");
+            file.deleteOnExit();
+
+            jar.write(file);
+            return file;
+        }
+        finally {
+            b.close();
+        }
+    }
+
+    protected static File createPackage(String name, Version version, File... bundles) throws Exception {
+        DeploymentPackageBuilder builder = DeploymentPackageBuilder.createDeploymentPackage(name, version.toString());
+
+        OutputStream fos = null;
+        try {
+            for (File bundle : bundles) {
+                builder.addBundle(bundle.toURI().toURL());
+            }
+
+            File file = File.createTempFile("testpackage", ".jar");
+            file.deleteOnExit();
+
+            fos = new FileOutputStream(file);
+            builder.generate(fos);
+
+            return file;
+        }
+        finally {
+            if (fos != null) {
+                fos.close();
+            }
+        }
+    }
+
+    @Override
+    protected void configureProvisionedServices() throws Exception {
+        resetAgentBundleState();
+    }
+
+    protected void resetAgentBundleState() throws Exception {
+        Bundle agentBundle = m_bundleContext.getBundle();
+        File dataDir = m_bundleContext.getDataFile("");
+
+        // System.out.println("BaseAgentTest: Stopping agent bundle");
+        agentBundle.stop();
+        // System.out.println("BaseAgentTest: Cleaning bundle data dir (" + dataDir + ")");
+        cleanDir(dataDir);
+        // System.out.println("BaseAgentTest: Cleaning system properties");
+        Set<String> keysBeRemoved = new HashSet<String>();
+        for (Object key : System.getProperties().keySet()) {
+            if (key instanceof String && ((String) key).startsWith(AgentConstants.CONFIG_KEY_NAMESPACE)) {
+                keysBeRemoved.add((String) key);
+            }
+        }
+        for (String removeKey : keysBeRemoved) {
+            System.clearProperty(removeKey);
+        }
+        // System.out.println("BaseAgentTest: Starting agent bundle");
+        agentBundle.start();
+    }
+
+    protected void configureAgent(ConfigurationHandler handler, String... configuration) {
+        Map<String, String> config = new HashMap<String, String>();
+        for (int i = 0; i < configuration.length; i += 2) {
+            config.put(configuration[i], configuration[i + 1]);
+        }
+        handler.putAll(config);
+    }
+
+    private void cleanDir(File dir) {
+        if (!dir.isDirectory()) {
+            throw new IllegalStateException();
+        }
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                cleanDir(file);
+            }
+            file.delete();
+        }
+        dir.delete();
+    }
+}
