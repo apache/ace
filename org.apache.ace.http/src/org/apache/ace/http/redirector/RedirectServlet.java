6057f03eab05b0f5409ec411f7bac14f21cc04e7
Marcel Offermans <marrs@apache.org =  marrs = Marcel Offermans marrs@apache.org@apache.org>
2014-04-15 08:06:27 +0000

    Added license headers. Removed some old versions of libraries. Preparing for release.
    git-svn-id: https://svn.apache.org/repos/asf/ace/trunk@1587471 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.ace.http/src/org/apache/ace/http/redirector/RedirectServlet.java
+++ org.apache.ace.http/src/org/apache/ace/http/redirector/RedirectServlet.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.ace.http.redirector;
 
 import java.io.IOException;
@@ -17,7 +35,8 @@ import org.osgi.service.cm.ConfigurationException;
  * it will instead show you the redirect (and you can still click on the target URL).
  */
 public class RedirectServlet extends HttpServlet {
-    public static final String REDIRECT_URL_KEY = "org.apache.ace.webui.vaadin.redirect";
+	private static final long serialVersionUID = -2218040475620089019L;
+	public static final String REDIRECT_URL_KEY = "org.apache.ace.webui.vaadin.redirect";
     private final Object LOCK = new Object();
     private volatile ServiceRegistration m_registration;
     private String m_redirectURL;
