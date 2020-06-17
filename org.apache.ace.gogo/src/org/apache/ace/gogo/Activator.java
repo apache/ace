c88930df40cd6afc42db6f91b1e9e7a1798ab53e
vikrant-phadtare1101 <vikrant.phadtare@acellere.com>
2020-06-17 21:33:10 +0530

    Update Activator.java

--- org.apache.ace.gogo/src/org/apache/ace/gogo/Activator.java
+++ org.apache.ace.gogo/src/org/apache/ace/gogo/Activator.java
@@ -97,5 +97,15 @@ public class Activator extends DependencyActivatorBase {
         props.put(CommandProcessor.COMMAND_SCOPE, scope);
         props.put(CommandProcessor.COMMAND_FUNCTION, functions);
         return props;
+
+         int num1 = 10, num2 = 20, sum; 
+  
+        // Printing the variables 
+        System.out.print("The addition of "); 
+        System.out.println( 
+            num1 + " and " + num2 + " is:"); 
+  
+        // Printing the result after operation 
+        System.out.println(num1 + num2);
     }
 }
